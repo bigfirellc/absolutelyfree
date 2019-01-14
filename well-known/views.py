11e674ba16b96eb0ac8131ab4a80ce2c25bec03f
Adam Copeland <adamcopeland@protonmail.com>
2019-01-14 17:01:10 -0500

    Adding well-known directory for certbot

---
+++ well-known/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+from .models import Bandname
+from django.template import loader
+import random
+
+def index(request):
+    #full_bandname_list = Bandname.objects.order_by('-pub_date')
+    template = loader.get_template('well-known/index.html')
+    return HttpResponse(template.render(context, request))
