3a93ac9b4d10700d571c1de49e0a04c96a04e1d3
Adam Copeland <adamcopeland@protonmail.com>
2020-05-06 20:32:25 -0400

    Adding album name model

--- rand/models.py
+++ rand/models.py
@@ -14,3 +14,16 @@ class Bandname(models.Model):
 
     def roll_dice(self):
         return self.count()
+
+class Album(models.Model):
+    name = models.CharField(max_length=1024)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.bandname_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def roll_dice(self):
+        return self.count()      
