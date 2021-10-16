d2c678db537106610e73ceec4e033efb25cf6e0f
Adam Copeland <lesservehicle@protonmail.com>
2021-10-15 22:51:42 -0400

    searchpage

--- rand/urls.py
+++ rand/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('jumble/', views.jumble),
     path('album/', views.albums),
-    path('search/', views.SearchResultsView)
+    path('search/', views.SearchResultsView.as_view(), name='search_results')
 ]
