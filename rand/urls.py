96a5f1b0dad38dd0918001382e2078223b64853b
Adam Copeland <lesservehicle@protonmail.com>
2021-10-17 16:44:02 -0400

    search page bugfix

--- rand/urls.py
+++ rand/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('jumble/', views.jumble),
     path('album/', views.albums),
-    path('results/', views.SearchResultsView.as_view(), name='search_results')
-    path('search/', SearchPageView.as_view(), name='search'),
+    path('results/', views.SearchResultsView.as_view(), name='search_results'),
+    path('search/', SearchPageView.as_view(), name='search')
 ]
