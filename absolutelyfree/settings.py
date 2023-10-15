8f163782ca145c07e9dfe4fbe53033a9283c836e
Adam Copeland <adam@lesservehicle.com>
2023-10-15 13:38:49 -0400

    settings fix

--- absolutelyfree/settings.py
+++ absolutelyfree/settings.py
@@ -129,7 +129,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+    '/home/app/web/staticfiles/'
+]
+
 STATIC_URL = '/static/'
 
 JET_THEMES = [
